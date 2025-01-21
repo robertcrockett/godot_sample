eb03e63a9f183d20a43055a038d6010434a48c13
robertcrockett <robertwcrockett@hotmail.com>
2025-01-20 19:07:18 -0500

    Remove debugging line

--- HUD.cs
+++ HUD.cs
@@ -8,9 +8,7 @@ public partial class HUD : CanvasLayer
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
-	{
-		Console.WriteLine("HUD Ready");
-	}
+	{ }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
